ac7dac2e4e9e08485e1657e039af24afc8f877ed
AljureS <saidsimon2@gmail.com>
2024-09-11 11:08:16 -0500

    Backup

---
+++ src/main/java/com/said_market/persistence/entities/Compra.java
@@ -0,0 +1,75 @@
+package com.said_market.persistence.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "compras")
+public class Compra {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idCompra")
+    private Integer idCompra;
+
+    @Column(name = "id_cliente")
+    private String idCliente;
+
+    private LocalDateTime fecha;
+
+    @Column(name = "medio_pago")
+    private String medioPago;
+
+    private String comentario;
+    
+    private String estado;
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public String getMedioPago() {
+        return medioPago;
+    }
+    public void setMedioPago(String medioPago) {
+        this.medioPago = medioPago;
+    }
+}
---
+++ src/main/java/com/said_market/persistence/entities/Product.java
@@ -0,0 +1,85 @@
+package com.said_market.persistence.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "productos")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Integer idProducto;
+
+    private String nombre;
+
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+
+    @Column(name = "codigo_barras")
+    private String codigoBarras;
+
+    @Column(name = "precio_venta")
+    private Double precioVenta;
+
+    @Column(name = "cantidad_stock")
+    private Integer cantidadStock;
+
+    private Boolean estado;
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+    }
+    public String getCodigoBarras() {
+        return codigoBarras;
+    }
+
+    public void setCantidadStock(Integer cantidadStock) {
+        this.cantidadStock = cantidadStock;
+    }
+    public Integer getCantidadStock() {
+        return cantidadStock;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setPrecioVenta(Double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+    public Double getPrecioVenta() {
+        return precioVenta;
+    }
+
+
+}
