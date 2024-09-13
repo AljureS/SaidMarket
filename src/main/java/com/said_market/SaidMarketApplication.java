6bc92157ecf3da1215a452ad59ea891b858b8d22
AljureS <saidsimon2@gmail.com>
2024-09-12 19:47:25 -0500

    For today

--- src/main/java/com/said_market/SaidMarketApplication.java
+++ src/main/java/com/said_market/SaidMarketApplication.java
@@ -1,12 +1,20 @@
 package com.said_market;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class SaidMarketApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
+		System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
+		System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(SaidMarketApplication.class, args);
 		
 	}
--- src/test/java/com/said_market/SaidMarketApplicationTests.java
+++ src/test/java/com/said_market/SaidMarketApplicationTests.java
@@ -1,11 +1,20 @@
 package com.said_market;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootTest
 class SaidMarketApplicationTests {
 
+	@BeforeAll
+	static void setup(){
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
+		System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
+		System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
+	}
 	@Test
 	void contextLoads() {
 	}
